2db6229157c257ab068c0eb6966f523225ecea93
RedContritio <RedContritio@qq.com>
2022-04-06 22:03:20 +0800

    update train

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,11 @@ DATA_PROCESSED_DIR = os.path.join(DATA_BASE_DIR, 'processed')
 
 DATA_MODEL_DIR = os.path.join(DATA_BASE_DIR, 'model')
 
-DATA_W2V_DIR = os.path.join(DATA_MODEL_DIR, 'w2v')
+DATA_TEXT_SEQUENCES_PATH = os.path.join(DATA_PROCESSED_DIR, 'text_sequences.pkl')
+
+DATA_LABELS_PATH = os.path.join(DATA_PROCESSED_DIR, 'labels.pkl')
+
+DATA_W2V_DIR = os.path.join(DATA_PROCESSED_DIR, 'w2v')
 
 DATA_W2V_VECTOR_PATH = os.path.join(DATA_W2V_DIR, 'vector.tsv')
 
@@ -17,3 +21,12 @@ DATA_W2V_META_PATH = os.path.join(DATA_W2V_DIR, 'metadata.tsv')
 for (k, v) in list(locals().items()):
     if k.endswith('_DIR') and not os.path.exists(v):
         os.mkdir(v)
+
+class CustomIterator:
+    def __init__(self, dataset, operator = lambda x: x):
+        self.dataset = dataset
+        self.operator = operator
+    
+    def __iter__(self):
+        for line in self.dataset:
+            yield self.operator(line)
